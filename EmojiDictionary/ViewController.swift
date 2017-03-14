d05fbe3c3107f44849374019de0498c37339df8a
Kyle Reddeman <kylereddeman@D-10-157-49-230.dhcp4.washington.edu>
2017-03-13 17:56:16 -0700

    initial commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by Kyle Reddeman on 3/13/17.
+//  Copyright © 2017 Kyle Reddeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var emojiArray = ["💀", "😴", "😈", "👺"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(emojiArray[indexPath.row])"
+        return cell
+    }
+
+}
+
